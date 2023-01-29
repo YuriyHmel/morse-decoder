cb5ca2aa1dedaa563812fb700530e504462fe04e
YuriyHmel <hmelnich@yandex.ru>
2023-01-29 23:31:07 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    for (let key in MORSE_TABLE) {
+
+    }
+    let arrExpr = expr.match(/.{1,2}/g);
+    let result = [];
+    for (let j = 0; j < arrExpr.length; j = j + 5) {
+        let str = '';
+        for (let i = j; i < j + 5; i++){
+            switch (arrExpr[i]) {
+                case '11': arrExpr[i] = '-';
+                str = `${str}${'-'}`;
+                break;
+                case '10': arrExpr[i] = '.';
+                str = `${str}${'.'}`;
+                break;
+                case '**': arrExpr[i] = '**';
+                str = '**';
+                break;
+            }   
+        }
+        if (str === '**') {
+            result.push(' ');
+        }
+            for (let key in MORSE_TABLE) {
+                if (key === str) {
+                    result.push(MORSE_TABLE[key]);
+                }
+        }
+    }
+    return result.join('');
 }
 
 module.exports = {
